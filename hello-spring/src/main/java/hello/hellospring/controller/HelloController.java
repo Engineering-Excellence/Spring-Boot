4e8663032ae08d48f4940747f41633c1101d826c
Kyle <prophet@hanyang.ac.kr>
2023-11-10 23:48:28 +0900

    스프링 입문 - Static, Templates, MVC

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,13 +3,22 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping(value = "")
 public class HelloController {
 
     @GetMapping(value = "/hello")
     public String hello(Model model) {
         model.addAttribute("data", "spring!!");
         return "hello";
+    }
+
+    @GetMapping(value = "/hello-mvc")
+    public String helloMvc(@RequestParam(name = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
